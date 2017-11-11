ff5e0295756f52893174fb8cb5f374284cc285ff
Candalk <2426170744@qq.com>
2017-11-11 12:57:45 +0800

    最优服务次序问题

--- 4-6最优服务次序问题.cpp
+++ 4-6最优服务次序问题.cpp
@@ -1,3 +1,10 @@
+// 4-6最优服务次序问题.cpp : 定义控制台应用程序的入口点。
+//
+/*
+10
+56 12 1 99 1000 234 33 55 99 812
+*/
+#include "stdafx.h"
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -6,18 +13,22 @@ int main()
 {
 
 
-	int N,sum=0,wait=0;
-	cin>>N;
+	int N, sum = 0, wait = 0;
+	cin >> N;
 	vector<int> v(N);
-	for(int i=0;i<N;i++)
-		cin>>v[i];
-	sort(v.begin(),v.end());
-	for(int i=0;i<N;i++){
-		wait+=v[i];
-		sum+=wait;
-		
+	for (int i = 0; i<N; i++)
+		cin >> v[i];
+	sort(v.begin(), v.end());
+	/*
+	从小到大排序，先服务服务时间少的顾客
+	wait表示当前顾客需要等待的时间
+	*/
+	for (int i = 0; i<N; i++) {
+		wait += v[i];
+		sum += wait;
+
 	}
 
-	printf("sum = %d\n",sum);
-	printf("%0.2f",sum*1.0/N);               
+	printf("sum = %d\n", sum);
+	printf("%0.2f", sum*1.0 / N);
 }
