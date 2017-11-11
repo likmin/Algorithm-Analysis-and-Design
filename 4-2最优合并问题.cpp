69161d98d66bdf73f5764dabd197cc33f73b948e
Candalk <2426170744@qq.com>
2017-11-11 12:48:16 +0800

    4-2最优合并问题

--- 4-2最优合并问题.cpp
+++ 4-2最优合并问题.cpp
@@ -9,45 +9,49 @@ bool cmp(int a, int b) {
 }
 int main()
 {
-	int N,minsum=0,maxsum=0;
+	int N, minsum = 0, maxsum = 0;
 	cin >> N;
 	/*
-	v1æ•°ç»„ç”¨äºŽå¯»æ‰¾æœ€å°å€¼
-	v2æ•°ç»„ç”¨äºŽå¯»æ‰¾æœ€å¤§å€¼
+	v1,v2Êý×éÓÃÓÚ´æ´¢Ã¿¸ök¸ö´ýºÏ²¢ÐòÁÐµÄ³¤¶È
+	v1Êý×éÓÃÓÚÑ°ÕÒ×îÐ¡Öµ
+	v2Êý×éÓÃÓÚÑ°ÕÒ×î´óÖµ
 	*/
-	vector<int> v1(N),v2(N);
+	vector<int> v1(N), v2(N);
 	for (int i = 0; i < N; i++) {
 		cin >> v1[i];
 		v2[i] = v1[i];
 	}
-		/*
-		ä»Žå°åˆ°å¤§å¯¹v1è¿›è¡ŒæŽ’åº
-		åªç›¸åŠ æœ€å°çš„
-		*/
+	/*
+	´ÓÐ¡µ½´ó¶Ôv1½øÐÐÅÅÐò
+	Ö»Ïà¼Ó×îÐ¡µÄ
+	*/
 	sort(v1.begin(), v1.end());
 	int i = 1;
 	while (i < N) {
-		int temp = v1[i-1] + v1[i];
-		minsum += temp-1;
+		/*
+		2Â·ºÏ²¢Ëã·¨ºÏ²¢Á½¸ö³¤¶È·Ö±ðÎªmºÍnµÄÐòÁÐÐèÒªm+n-1´Î±È½Ï
+		*/
+		int temp = v1[i - 1] + v1[i];
+		minsum += temp - 1;
 		v1[i] = temp;
 		sort(v1.begin() + i, v1.end());
 		i++;
 	}
-	printf("minsum = %d", minsum);
+	printf("minsum = %d\n", minsum);
 
 	/*
-	ä»Žå°åˆ°å¤§å¯¹v2è¿›è¡ŒæŽ’åº
-	åªç›¸åŠ æœ€å¤§çš„
+	´ÓÐ¡µ½´ó¶Ôv2½øÐÐÅÅÐò
+	Ö»Ïà¼Ó×î´óµÄ
 	*/
-	sort(v2.begin(), v2.end(),cmp);
-	 i = 1;
+	sort(v2.begin(), v2.end(), cmp);
+	i = 1;
 	while (i < N) {
 		int temp = v2[i - 1] + v2[i];
-		maxsum += temp-1;
+		maxsum += temp - 1;
 		v2[i] = temp;
-		sort(v2.begin() + i, v2.end(),cmp);
+		sort(v2.begin() + i, v2.end(), cmp);
 		i++;
 	}
 	printf("maxsum = %d", maxsum);
-    return 0;
+	return 0;
 }
